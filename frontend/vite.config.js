a7f11558cadda66443670d6926a7ec9b7cf4bb3d
eric <siaumau@gmail.com>
2025-07-14 17:05:49 +0800

    chore: update Vite configuration to disable HTTPS and remove unused SSL plugin
    - Disabled HTTPS in the Vite server configuration for local development.
    - Removed the basic SSL plugin as it is no longer needed.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    basicSsl()
+    vueDevTools()
   ],
   resolve: {
     alias: {
@@ -20,7 +18,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    https: true,
+    https: false,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
