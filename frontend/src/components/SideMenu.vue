098a7998c44b4cf32e21cec77f5c78c6f8fbfb21
eric <siaumau@gmail.com>
2025-07-14 15:31:25 +0800

    style: 動態調整 QR Code 標題及副標題字體大小以適應櫃位類型
    - 在 LocationsView.vue 中根據存放類型新增 CSS 類別，縮小櫃位類型的 QR Code 標題和副標題字體大小，提升列印效果及可讀性。
    - 確保不同存放類型的顯示一致性，改善使用者體驗。

--- frontend/src/components/SideMenu.vue
+++ frontend/src/components/SideMenu.vue
@@ -152,6 +152,11 @@ const navigateTo = (path) => {
             <svg v-else-if="item.icon === 'permissions'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
             </svg>
+
+            <!-- 產品管理圖標 -->
+            <svg v-else-if="item.icon === 'products'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+            </svg>
           </span>
 
           <!-- 選單文字 -->
